41c9be67c0ea93772fa3a0c852ef0f59969310d3
AbdullahLaham <abed26194@gmail.com>
2023-09-13 00:20:12 +0300

    changes

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const dbConnect = () => {
     try {
-        const conn = mongoose.connect(process.env.MONGODB_URL);
+        const conn = mongoose.connect('mongodb+srv://abed26194:C45S7mi9R3PNgQSR@cluster0.2w2kaaf.mongodb.net/');
         console.log("Database Connected Successully");
     } catch (error) {
         console.log("Database Error");
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import dotenv from 'dotenv/config';
 import { dbConnect } from './config/dbConnect.js';
 import bodyParser from 'body-parser';
 import todoRouter from './routes/todoListRoute.js'
@@ -11,17 +10,16 @@ const app = express();
 app.use(bodyParser.json({"limit": "30mb", extended: true})); // 30mb because we will send images
 app.use(bodyParser.urlencoded({"limit": "30mb", extended: true}));
 app.use(cors())
+
 const PORT = process.env.PORT || 4000;
-// app.use('/', (req, res) => {
-//     res.send('server is running')
-// });
+
 
 app.use('/todo', todoRouter);
 
 
-// app.use('/', async (req, res) => {
-//     res.json("Server is running")
-// });
+app.use('/', async (req, res) => {
+    res.json("Server is running")
+});
 
 
 let server = app.listen(PORT, () => {
